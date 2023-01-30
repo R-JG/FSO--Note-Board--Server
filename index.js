5d971a74a2327d252a2af9f1615e61b7f47df38a
Robin <robin.james.guerrier@gmail.com>
2023-01-29 21:27:02 -0500

    Add get note at id response.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const notes = require('./db.json');
+const { notes } = require('./db.json');
 
 const express = require('express');
 const app = express();
@@ -8,8 +8,20 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>');
 });
 
+// get all notes
 app.get('/api/notes', (request, response) => {
     response.json(notes);
+});
+
+// get note at id
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const note = notes.find(note => note.id === id);
+    if (note) {
+        response.json(note);
+    } else {
+        response.status(404).end();
+    };
 });
 
 app.listen(PORT, () => {
