edb1d5b95320d917a6c472f0c437bbd15e62a001
Robin <robin.james.guerrier@gmail.com>
2023-01-29 20:38:16 -0500

    Setup basic express server responses for / and /api/notes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+const notes = require('./db.json');
+
+const express = require('express');
+const app = express();
+const PORT = 3003;
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>');
+});
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}...`);
+});
